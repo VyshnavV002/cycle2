a27c724edbf0bad49e299b539400fc00f78586a6
VyshnavV002 <97964414+VyshnavV002@users.noreply.github.com>
2022-01-25 21:54:23 +0530

    Add files via upload

---
+++ StringPal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class StringPal
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		String name,rev="";
+		System.out.println("Enter the word:");
+		name=sc.nextLine();
+		
+		int n=name.length();
+		for(int i=0;i<n;i++)
+		{
+			rev+=name.charAt(n-1-i);
+		}
+		if(name.equals(rev))
+		{
+			System.out.println(name+" is a palindrome");
+		}
+		else
+		{
+			System.out.println(name+" is not palindrome");
+		}
+		
+	}
+}
